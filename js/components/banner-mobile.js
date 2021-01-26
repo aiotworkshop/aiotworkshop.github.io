15e8061cecd240a34a8d3666c1ab1d63012bb742
Bo Frank <bo@bofrank.com>
2021-01-25 22:00:12 -0800

    Standardizing formating.

--- js/components/banner-mobile.js
+++ js/components/banner-mobile.js
@@ -9,11 +9,11 @@ document.getElementById('banner-mobile').innerHTML=`
             <div class="logo_content">
                 <div class="logo_text logo_text_not_ie">
                     The 3rd International Workshop on Artificial Intelligence of Things
-                    </div>
-                    <div class="logo_link_mobile">
+                </div>
+                <div class="logo_link_mobile">
                     <a href="https://www.kdd.org/kdd2020/" target="_new">In conjunction with the 26th ACM SIGKDD International Conference on Knowledge Discovery and Data Mining (KDD 2020)</a>
-                    </div>
-                    <div class="logo_sub">
+                </div>
+                <div class="logo_sub">
                     August 24th, 2020 - San Diego, California – USA test
                 </div>
             </div>
