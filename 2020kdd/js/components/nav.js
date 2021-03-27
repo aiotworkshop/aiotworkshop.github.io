d1a3698c26caa6d057ac4e48c8c90074c3be812c
Bo Frank <bo@bofrank.com>
2021-03-27 15:28:46 -0700

    Updating nav for archived.

--- 2020kdd/js/components/nav.js
+++ 2020kdd/js/components/nav.js
@@ -20,6 +20,8 @@ document.getElementById('nav').innerHTML=`
                                                 <i class="fa fa-angle-down"></i>
                                             </div>
                                             <div class="dropdown-content">
+                                                <a href="../index.html">KDD 2021</a>
+                                                <a href="../2020kdd/index.html">KDD 2020</a>
                                                 <a href="../2019/index.html">KDD 2019</a>
                                             </div>
                                         </div> 
