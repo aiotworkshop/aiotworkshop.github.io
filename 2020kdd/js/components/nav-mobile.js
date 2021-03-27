6a6fda10448d74e741bd7229e61bea904a26b887
Bo Frank <bo@bofrank.com>
2021-03-27 12:58:56 -0700

    Copying files.

---
+++ 2020kdd/js/components/nav-mobile.js
@@ -0,0 +1,14 @@
+document.getElementById('nav-mobile').innerHTML=`
+
+<ul>
+    <li class="menu_item_mobile"><a href="index.html">Home</a></li>
+    <li class="menu_item_mobile"><a href="CFP.html">Call For Papers</a></li>
+    <li class="menu_item_mobile"><a href="accepted-papers.html">Accepted Papers</a></li>
+    <li class="menu_item_mobile"><a href="organizers.html">Organizers</a></li>
+    <li class="menu_item_mobile"><a href="program.html">Program</a></li>
+    <!-- <li class="menu_item_mobile"><a href="keynote.html">Keynote</a></li> -->
+    <li class="menu_item_mobile"><a href="2019/index.html">KDD 2019</a></li>
+    <li class="menu_item_mobile"><a href="2020/index.html">AAAI 2020</a></li>
+</ul>
+
+`;
---
+++ 2020kdd/js/components/nav.js
@@ -0,0 +1,36 @@
+document.getElementById('nav').innerHTML=`
+
+<div class="header_nav" id="header_nav_pin">
+    <div class="header_nav_inner">
+        <div class="header_nav_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="header_nav_content d-flex flex-row align-items-center justify-content-start">
+                            <nav class="main_nav">
+                                <ul>
+                                    <li id="home"><a href="index.html">Home</a></li>
+                                    <li id="cfp"><a href="CFP.html">Call For Papers</a></li>
+                                    <li id="papers"><a href="accepted-papers.html">Accepted Papers</a></li>
+                                    <li id="organizers"><a href="organizers.html">Organizers</a></li>
+                                    <li id="program"><a href="program.html">Program</a></li>
+                                    <!-- <li><a href="keynote.html">Keynote</a></li> -->
+                                    <li><a href="2019/index.html">KDD 2019</a></li>
+                                    <li><a href="2020/index.html">AAAI 2020</a></li>
+                                </ul>
+                            </nav>
+
+                            <div class="header_extra ml-auto">
+                                <div class="button header_button">
+                                <a href="http://www.cvent.com/events/kdd-2020/event-summary-eb9eeca9963443d18902dbe47190ea4c.aspx">Register</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+`;
