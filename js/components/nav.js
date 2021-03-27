9b81e541243abc46d2104ac45237043cef515a48
Bo Frank <bo@bofrank.com>
2021-03-27 15:31:54 -0700

    Adding drop down to home page.

--- js/components/nav.js
+++ js/components/nav.js
@@ -15,7 +15,18 @@ document.getElementById('nav').innerHTML=`
                                     <li id="organizers"><a href="organizers.html">Organizers</a></li>
                                     <li id="program"><a href="program.html">Program</a></li>
                                     <!-- <li><a href="keynote.html">Keynote</a></li> -->
-                                    <li><a href="2019/index.html">KDD 2019</a></li>
+                                    <li>
+                                        <div class="dropdown">
+                                            <div class="dropbtn">KDD 2021
+                                                <i class="fa fa-angle-down"></i>
+                                            </div>
+                                            <div class="dropdown-content">
+                                                <a href="index.html">KDD 2021</a>
+                                                <a href="2020kdd/index.html">KDD 2020</a>
+                                                <a href="2019/index.html">KDD 2019</a>
+                                            </div>
+                                        </div> 
+                                    </li>
                                     <li><a href="2020/index.html">AAAI 2020</a></li>
                                 </ul>
                             </nav>
