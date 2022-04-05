e5b0f6e6be607e0c94757b1b2d4894a70138fe41
Bo Frank <bo@bofrank.com>
2022-04-05 16:36:39 +0900

    Updating titles and sponsors.

--- js/components/banner-mobile.js
+++ js/components/banner-mobile.js
@@ -11,10 +11,10 @@ document.getElementById('banner-mobile').innerHTML=`
                     The 5th International Workshop on Artificial Intelligence of Things
                 </div>
                 <div class="logo_link_mobile">
-                    <a href="https://www.kdd.org/kdd2022/" target="_new">In conjunction with the 28th ACM SIGKDD International Conference on Knowledge Discovery and Data Mining (KDD 2022)</a>
+                    <a href="https://www.kdd.org/kdd2022/" target="_new">In conjunction with the 28th ACM SIGKDD Conference on Knowledge Discovery and Data Mining (KDD 2022)</a>
                 </div>
                 <div class="logo_sub">
-                    Washington DC, USA, Aug. 14th-18th(TBD), 2022
+                    Washington DC Convention Center, USA, Aug. 14th-18th(TBD), 2022
                 </div>
             </div>
         </div>
