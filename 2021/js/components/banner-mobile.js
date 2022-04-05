d0c6dac0b8ff8ccd209375c290c5a2f18462ee24
Bo Frank <bo@bofrank.com>
2022-04-05 11:03:59 +0900

    Updating image links.

--- 2021/js/components/banner-mobile.js
+++ 2021/js/components/banner-mobile.js
@@ -4,7 +4,7 @@ document.getElementById('banner-mobile').innerHTML=`
     <a href="index.html">
         <div class="logo_container d-flex flex-row align-items-start justify-content-start">
             <div class="logo_image">
-                <div><img src="images/logo.png" alt="" /></div>
+                <div><img src="../images/logo.png" alt="" /></div>
             </div>
             <div class="logo_content">
                 <div class="logo_text logo_text_not_ie">
